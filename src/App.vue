e51d70746ef05156a5487ff19f4dcfddec37142c
bruno-bento <brunobentoreinoso14@gmail.com>
2022-07-11 09:10:06 -0300

    subindo o projeto

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <header>
+    <nav class="flex items-center justify-between bg-main-blue p-5" >
+        <div class="w-28 lg:w-36">
+          <p class="text-main-yellow font-bold">
+            Logo
+          </p>
+        </div>
+        <div class="flex items-center text-white  font-regular ">
+          <ul class="flex gap-8 list-none mr-4">
+            <li>Sobre</li>
+            <li>Experiência</li>
+            <li>Habilidades</li>
+            <li>Projetos</li>
+          </ul>
+          <button class="rounded-md bg-main-yellow text-main-blue font-bold px-6 py-2">
+            Contato
+          </button>
+        </div>
+    </nav>
+  </header>
+</template>
+
+<script >
+</script>
