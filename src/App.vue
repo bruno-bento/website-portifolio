15df5db79770188e7b5475710edf8be2b1816b02
bruno-bento <brunobentoreinoso14@gmail.com>
2022-07-11 12:49:38 -0300

    Hero and nav basic

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,27 @@
 <template>
-  <Nav></Nav>
-  <Hero></Hero>
+  <div class="h-screen bg-main-blue">
+    <Nav></Nav>
+    <Hero></Hero>
+  </div>
+  <div class="h-screen bg-white">
+    <div class="flex justify-center">
+      <div>Foto</div>
+      <div>Sobre</div>
+    </div>
+    <div class="flex  justify-center">
+      <div>Habilidades</div>
+      <div>Habilidades</div>
+    </div>
+  </div>
 </template>
 
-<script >
-import Nav from './components/Nav/index.vue'
-import Hero from './components/Hero/index.vue'
+<script>
+import Nav from "./components/Nav/index.vue";
+import Hero from "./components/Hero/index.vue";
 export default {
   components: {
     Nav,
-    Hero
-  }
-}
+    Hero,
+  },
+};
 </script>
