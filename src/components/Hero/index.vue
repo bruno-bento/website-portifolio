14ea6a75904ac4b7aba1f8516db7c6de9d552c66
bruno-bento <brunobentoreinoso14@gmail.com>
2022-07-11 16:10:02 -0300

    melhoria estrutural

--- src/components/Hero/index.vue
+++ src/components/Hero/index.vue
@@ -1,20 +1,23 @@
 <template>
-  <section class=" w-full h-full flex justify-around items-center ">
-    <div class="w-2/5 ">
-      <h1 class="text-main-yellow font-bold mb-4 text-4xl">
-        Bruno Bento <br/> Desenvolvedor Front-End
-      </h1>
-      <button
-        class="rounded-md bg-main-yellow text-main-blue font-bold px-6 py-2"
-      >
-        Contato
-      </button>
-    </div>
-    <div class="w-2/5 ">
-      <img
-        src="../../assets/images/undraw_code_review_re_woeb 1.svg"
-        alt="MyPicture"
-      />
+  <section class="w-full h-full flex justify-center items-center">
+    <div class="flex justify-around w-5/6 items-center">
+      <div class="w-3/5">
+        <h1 class="text-main-yellow font-bold mb-4 text-4xl">
+          Bruno Bento <br />
+          Desenvolvedor Front-End
+        </h1>
+        <button
+          class="rounded-md bg-main-yellow text-main-blue font-bold px-6 py-2"
+        >
+          Contato
+        </button>
+      </div>
+      <div class="w-2/5">
+        <img
+          src="../../assets/images/undraw_code_review_re_woeb 1.svg"
+          alt="MyPicture"
+        />
+      </div>
     </div>
   </section>
 </template>
