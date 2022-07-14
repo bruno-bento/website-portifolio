cd5afdcfd84ef4912cd54e6b83c5b6e7d1a5da30
bruno-bento <brunobentoreinoso14@gmail.com>
2022-07-14 08:25:23 -0300

    Sub hero update

--- src/components/SubHero/index.vue
+++ src/components/SubHero/index.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="flex justify-center items-center w-full h-full">
     <div class="flex flex-col justify-around items-center h-5/6 w-5/6">
-      <div class="flex justify-between items-center w-full gap-5 h-5/6 ">
+      <div class="flex justify-between items-center w-full gap-5 h-5/6">
         <div class="flex items-center w-2/6">
           <img
             class="rounded-md w-4/6 h-4/6 filter shadow"
@@ -9,16 +9,24 @@
             alt="profilePicture"
           />
         </div>
-        <div class="flex justify-center w-4/6 h-4/6 bg-main-yellow rounded-md">
-            <p class="font-bold text-main-blue mt-4">
-                Sobre   
-            </p>
+        <div class="flex flex-col items-center w-4/6 h-4/6 bg-main-yellow rounded-md" >
+          <p class="font-bold text-main-blue mt-4 text-2xl">Sobre</p>
+          <p class="font-regular text-main-blue mt-4 text-center text-lg">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, culpa qui officia deserunt mollit anim id est
+            laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Ut enim ad minim veniam, culpa qui officia deserunt mollit anim id
+            est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing
+            elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+            aliqua. Ut enim ad minim veniam, culpa qui officia deserunt mollit
+            anim id est laborum.
+          </p>
         </div>
       </div>
-      <div class="flex justify-center bg-main-blue w-full rounded items-center h-2/6">
-        <div class="flex justify-center">
-          <div class="text-white font-regular" >Habilidades</div>
-        </div>
+      <div class="flex flex-col justify-start items-center bg-main-blue w-full rounded h-2/6">
+        <p class="text-white font-regular py-5 text-xl font-bold">Habilidades</p>
       </div>
     </div>
   </section>
