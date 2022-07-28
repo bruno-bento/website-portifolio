6c46617790281de4f72a87e52ba521937fe7d6ad
bruno-bento <brunobentoreinoso14@gmail.com>
2022-07-28 08:21:55 -0300

    change footer

--- src/components/Footer/index.vue
+++ src/components/Footer/index.vue
@@ -6,7 +6,7 @@
     <div class="flex justify-center items-center gap-10 px-5 w-full h-full bg-white">
         <div class="flex flex-col justify-evenly items-center w-4/6 h-4/6 " >
 
-    
+          <h1>Bruno</h1>
         </div>
     </div>
   </section>
